1ee5f3be1ba7c933641bbc154104b1a4f549bd76
Jimemart <jimena.mg@hotmail.com>
2017-10-10 18:52:26 +0200

    Ready to deploy

--- server/app.js
+++ server/app.js
@@ -73,6 +73,10 @@ app.use('/api', gamesRoutes)
 app.use('/api', singleGame)
 app.use('/api', checkDb)
 
+app.use((req, res, next) => {
+ res.sendfile(__dirname + '/public/index.html');
+});
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
